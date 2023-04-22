d8c14f7f3827ba2c72ae1fb8789161f141b176d4
Trung Nguyen <trungnguyenjr123@gmail.com>
2023-04-22 22:18:32 +0700

    update

--- realmadrid.js
+++ realmadrid.js
@@ -7,4 +7,30 @@ setInterval(function(){
         counter=1;
     }
 },1500)
-//List ảnh
+//Sign in / Sign up
+const formOpenBtn=document.querySelector('.form-open'),
+formContainer = document.querySelector('.form_container'),
+formCloseBtn = document.querySelector('.form_close'),
+signupBtn = document.querySelector('#signup'),
+signupLink=document.querySelector('.sign_up_btn'),
+loginBtn = document.querySelector('#login'),
+pwShowHide = document.querySelector('.pw_hide')
+
+formOpenBtn.addEventListener("click",()=> formContainer.classList.remove("opacity"));
+formCloseBtn.addEventListener("click",()=> formContainer.classList.add("opacity"))
+
+signupLink.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.add("active")
+    formContainer.classList.remove("opacity")
+})
+signupBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.add("active")
+})
+loginBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.remove("active")
+})
+
+
