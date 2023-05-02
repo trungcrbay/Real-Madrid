c5205d289cc47be5775bc884dbbeca4e15489fe7
Trung Nguyen <trungnguyenjr123@gmail.com>
2023-05-02 15:26:50 +0700

    update

--- realmadrid.js
+++ realmadrid.js
@@ -14,13 +14,22 @@ formCloseBtn = document.querySelector('.form_close'),
 signupBtn = document.querySelector('#signup'),
 signupLink=document.querySelector('.sign_up_btn'),
 loginBtn = document.querySelector('#login'),
-pwShowHide = document.querySelectorAll('.pw_hide')
+pwShowHide = document.querySelectorAll('.pw_hide'),
+body = document.querySelector('body')
 
-formOpenBtn.addEventListener("click",()=> formContainer.classList.remove("opacity"));
-formCloseBtn.addEventListener("click",()=> formContainer.classList.add("opacity"))
+formOpenBtn.addEventListener("click",()=> {
+    formContainer.classList.remove("opacity")
+    body.style.opacity = '0.8'
+    formContainer.style.opacity='1'
+});
+formCloseBtn.addEventListener("click",()=>{
+    formContainer.classList.add("opacity")
+    body.style.opacity = '1'
+})
 
 signupLink.addEventListener("click",(e)=>{
     e.preventDefault();
+    body.style.opacity = '0.8'
     formContainer.classList.add("active")
     formContainer.classList.remove("opacity")
 })
